badce63a1ab703f0d2da4e9ef75b5582771bfd70
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-01-31 09:25:21 -0300

    feat: add help command to command list

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -4,10 +4,12 @@ import { startCommand } from './start.js';
 import genCommand from './gen.js';
 import starsCommand from './stars.js';
 import balanceCommand from './balance.js';
+import helpCommand from './help.js';
 
 export function setupCommands(bot: Bot<BotContext>) {
   bot.use(startCommand);
   bot.use(genCommand);
   bot.use(starsCommand);
   bot.use(balanceCommand);
+  bot.use(helpCommand);
 }
