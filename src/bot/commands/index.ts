ec323d568814112159852b7653e58b8b54b4a4aa
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-01-31 08:59:50 -0300

    feat: add balance command to command list

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -3,9 +3,11 @@ import type { BotContext } from '../../types/bot.js';
 import { startCommand } from './start.js';
 import genCommand from './gen.js';
 import starsCommand from './stars.js';
+import balanceCommand from './balance.js';
 
 export function setupCommands(bot: Bot<BotContext>) {
   bot.use(startCommand);
   bot.use(genCommand);
   bot.use(starsCommand);
+  bot.use(balanceCommand);
 }
