c40719252322c50caaacc72f0832847566e3b5bb
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-01-31 13:16:30 -0300

    Fix server export

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,26 +3,28 @@ import cors from '@fastify/cors';
 import { config } from '../config';
 import { logger } from '../lib/logger';
 
-// Create server
-const server = fastify({
-  logger,
-});
+export async function setupServer() {
+  // Create server
+  const server = fastify({
+    logger,
+  });
 
-// Setup CORS
-await server.register(cors, {
-  origin: config.MINIAPP_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'x-user-id'],
-});
+  // Setup CORS
+  await server.register(cors, {
+    origin: config.MINIAPP_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'x-user-id'],
+  });
 
-// Start server
-try {
-  const port = parseInt(config.PORT);
-  await server.listen({ port });
-  logger.info({ port }, 'Server started');
-} catch (err) {
-  logger.error(err);
-  process.exit(1);
+  // Start server
+  try {
+    const port = parseInt(config.PORT);
+    await server.listen({ port });
+    logger.info({ port }, 'Server started');
+  } catch (err) {
+    logger.error(err);
+    process.exit(1);
+  }
+
+  return server;
 }
-
-export default server;
