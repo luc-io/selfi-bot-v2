e43bf3f0c24f336ad06ef32f3ff828cb351896d5
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-02-01 13:10:55 -0300

    fix: update command exports

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -1,15 +1,9 @@
-import { Bot } from 'grammy';
-import type { BotContext } from '../../types/bot.js';
 import { startCommand } from './start.js';
-import genCommand from './gen.js';
-import starsCommand from './stars.js';
-import balanceCommand from './balance.js';
-import helpCommand from './help.js';
+import gen from './gen.js';
+import balance from './balance.js';
 
-export function setupCommands(bot: Bot<BotContext>) {
-  bot.use(startCommand);
-  bot.use(genCommand);
-  bot.use(starsCommand);
-  bot.use(balanceCommand);
-  bot.use(helpCommand);
-}
+export const commands = {
+  start: startCommand,
+  gen,
+  balance,
+};
