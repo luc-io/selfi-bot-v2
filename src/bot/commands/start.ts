ddaf9f6e71ea3f1cf06735926426bb878dae96ac
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-02-03 14:17:03 -0300

    fix: add debug logging to start command

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -1,19 +1,32 @@
 import { Composer } from 'grammy';
 import { BotContext } from '../../types/bot.js';
 import { getOrCreateUser } from '../../lib/user.js';
+import { logger } from '../../lib/logger.js';
 
 const composer = new Composer<BotContext>();
 
 composer.command('start', async (ctx) => {
+  logger.info('Start command received');
+  
   if (!ctx.from) {
+    logger.warn('No from field in context');
     await ctx.reply('Could not identify user');
     return;
   }
 
-  const telegramId = ctx.from.id.toString();
-  const user = await getOrCreateUser(telegramId, ctx.from.username ?? undefined);
-
-  await ctx.reply(`Welcome! You have ${user.stars} ⭐`);
+  try {
+    const telegramId = ctx.from.id.toString();
+    logger.info({ telegramId, username: ctx.from.username }, 'Getting or creating user');
+    
+    const user = await getOrCreateUser(telegramId, ctx.from.username ?? undefined);
+    logger.info({ telegramId, stars: user.stars }, 'User retrieved/created');
+    
+    await ctx.reply(`Welcome! You have ${user.stars} ⭐`);
+    logger.info({ telegramId }, 'Welcome message sent');
+  } catch (error) {
+    logger.error('Error in start command:', error);
+    await ctx.reply('Sorry, something went wrong while processing your request.');
+  }
 });
 
 export default composer;
