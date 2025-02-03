03add73b3c57d8303170082bd8d7c044e5eeedae
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-02-03 14:40:33 -0300

    feat: register payment handlers

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -3,11 +3,13 @@ import { BotContext } from '../../types/bot.js';
 import startCommand from './start.js';
 import starsCommand from './stars.js';
 import genCommand from './gen.js';
+import payments from '../payments/index.js';
 
 const composer = new Composer<BotContext>();
 
 composer.use(startCommand);
 composer.use(starsCommand);
 composer.use(genCommand);
+composer.use(payments);
 
 export default composer;
