778e09ccdab7891d2b65e6962aaefe94bfbb3df3
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-02-04 11:05:28 -0300

    Update gen command to use composer

--- src/bot/commands/gen.ts
+++ src/bot/commands/gen.ts
@@ -1,20 +1,21 @@
-import { Bot } from "grammy";
-import { PrismaClient } from "@prisma/client";
+import { Composer } from "grammy";
 import { generateImage } from "../../services/generation.js";
 import { hasSubscription } from "../middlewares/subscription.js";
 import { handleError } from "../../utils/error.js";
-import { CommandContext } from "../../types/bot.js";
+import { BotContext } from "../../types/bot.js";
 
-export default function setupGenCommand(bot: Bot, prisma: PrismaClient) {
-  bot.command("gen", hasSubscription, async (ctx: CommandContext) => {
-    try {
-      const response = await generateImage({
-        prompt: ctx.message?.text || "",
-      });
+const composer = new Composer<BotContext>();
 
-      await ctx.replyWithPhoto(response.images[0].url);
-    } catch (error) {
-      handleError(ctx, error);
-    }
-  });
-}
+composer.command("gen", hasSubscription, async (ctx) => {
+  try {
+    const response = await generateImage({
+      prompt: ctx.message?.text?.replace(/^\/gen\s+/, "") || "",
+    });
+
+    await ctx.replyWithPhoto(response.images[0].url);
+  } catch (error) {
+    handleError(ctx, error);
+  }
+});
+
+export default composer;
